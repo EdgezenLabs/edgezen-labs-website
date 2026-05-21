185ac7f687b16d53b9b621f259db152b0f21d6ff
Judith <judith.blessy@edgezenlabs.com>
2026-05-21 15:38:00 +0530

    chore: update base path to root in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/edgezen-labs-website/",
+  base: "/",
 
   server: {
     host: "::",
